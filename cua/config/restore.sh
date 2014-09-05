36e31b697d6a88defa50966a1a81c438042883fa
Bryan Robbins <bryantrobbins@gmail.com>
2014-09-05 17:27:47 +0000

    Adding slaves to cua config

--- cua/config/restore.sh
+++ cua/config/restore.sh
@@ -4,11 +4,19 @@ archive=$1
  
 if [ -z "$archive" ]; then
   echo "No archive provided."
-  echo "Usage: $0 archive.tar"
+  echo "Usage: $0 archive.tar numberOfSlaves"
+  exit
+fi
+
+nslave=$2
+
+if [ -z "$nslave" ]; then
+  echo "Number of jenkins slaves not provided"
+  echo "Usage: $0 archive.tar numberOfSlaves"
   exit
 fi
  
 echo "Restoring from $archive"
  
 ./load.sh $archive
-./start.sh
+./start.sh $nslave
--- cua/config/start.sh
+++ cua/config/start.sh
@@ -1,9 +1,24 @@
 #/bin/bash
+
+nslave=$1
+
+if [ -z "$nslave" ]; then
+  echo "Number of jenkins slaves not provided"
+  echo "Usage: $0 numberOfSlaves"
+  exit
+fi
  
+# Build image for jenkins slaves
+docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave" | awk '{print $1}' | xargs docker stop
+docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave" | awk '{print $1}' | xargs docker rm
+docker rmi bryantrobbins/jslave
+
+docker build -t="bryantrobbins/jslave" ./slave
+
 # Start cuanexus
 docker stop cuanexus
 docker rm cuanexus
-docker run -d --name cuanexus --volumes-from cuadata -p 9081:8081 conceptnotfound/sonatype-nexus
+docker run -d --name cuanexus -h cuanexus --volumes-from cuadata -p 9081:8081 conceptnotfound/sonatype-nexus
 
 # Start cuamongo
 docker stop cuamongo
@@ -14,3 +29,12 @@ docker run -d --name cuamongo --volumes-from cuadata -p 37017:27017 -p 38017:280
 docker stop cuajenkins
 docker rm cuajenkins
 docker run -d --name cuajenkins --volumes-from cuadata --link cuanexus:cuanexus --link cuamongo:cuamongo -p 9080:8080 -u root jenkins
+
+# Start some jenkins slaves
+for i in `seq 1 $nslave`;
+do
+  echo $i
+  docker stop cuaslave-$i
+  docker rm cuaslave-$i
+  docker run -d --name cuaslave-$i --link cuajenkins:cuajenkins --link cuanexus:cuanexus --link cuamongo:cuamongo bryantrobbins/jslave
+done
