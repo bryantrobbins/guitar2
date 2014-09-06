a4825a6d983c627b2dcdb8f64f4685284825ebe0
Bryan Robbins <bryantrobbins@gmail.com>
2014-09-06 00:39:20 -0400

    Adding cua scripts

---
+++ cua/backup.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+ 
+archive=$1
+ 
+if [ -z "$archive" ]; then
+  echo "No archive provided."
+  echo "Usage: $0 archive.tar"
+  exit
+fi
+ 
+if [ -e "$archive" ]; then
+  echo "File $archive already exists. Please choose another name"
+  exit
+fi
+ 
+local=`pwd`
+echo "Backing up to $archive"
+ 
+echo "Exporting data current rdata container"
+docker rm backup
+docker run --name backup --volumes-from rdata -v $local:/backup busybox tar cvf /backup/$archive var/jenkins_home nexus
+docker rm backup
---
+++ cua/load.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+ 
+archive=$1
+ 
+if [ -z "$archive" ]; then
+  echo "No archive provided."
+  echo "Usage: $0 archive.tar"
+  exit
+fi
+ 
+echo "Loading from $archive"
+ 
+# Clean up any old stuff here
+docker rm rdata
+ 
+# Start new data container
+echo "Starting new  rdata container"
+docker run -d --name rdata -v /var/jenkins_home -v /nexus busybox true
+ 
+# Copy in files from old data container
+echo "Restoring old files into new rdata"
+docker rm restore
+docker run --name restore --volumes-from rdata -v $(pwd):/backup busybox tar xvf /backup/$archive
+docker rm restore
