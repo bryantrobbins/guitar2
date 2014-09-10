9dd7c941b07104b307ac0af3fab0e867d7b04c6d
Bryan Robbins <bryantrobbins@gmail.com>
2014-09-10 10:47:02 +0000

    Remove old files

--- config/docker-old/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DOCKER_HOST=$(/usr/local/bin/boot2docker ip 2>/dev/null):2375
-docker rmi bryantrobbins/jenkins
-docker build -t bryantrobbins/jenkins .
--- config/docker/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --name myjenkins -p 8080:8080 -v /var/jenkins_home jenkins:latest
-
