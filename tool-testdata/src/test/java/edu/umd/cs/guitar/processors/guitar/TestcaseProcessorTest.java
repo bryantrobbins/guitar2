6f0d771ea3272dd115bee24ab8d313d5d90a5e32
Bryan Robbins <bryantrobbins@gmail.com>
2014-10-24 01:22:55 -0400

    Fix file path in test

--- tool-testdata/src/test/java/edu/umd/cs/guitar/processors/guitar/TestcaseProcessorTest.java
+++ tool-testdata/src/test/java/edu/umd/cs/guitar/processors/guitar/TestcaseProcessorTest.java
@@ -1,9 +1,11 @@
 package edu.umd.cs.guitar.processors.guitar;
 
+import com.google.common.io.Resources;
 import edu.umd.cs.guitar.model.data.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,9 +16,10 @@ public class TestcaseProcessorTest {
         TestcaseProcessor proc = new TestcaseProcessor();
 
         Map<String, String> options = new HashMap<String, String>();
-        options.put(TestcaseProcessor.FILE_PATH_OPTION,
-                "tool-testdata/src/test/resources" +
-                        "/t_e802842950_e103778092.tst");
+
+        URL tcFile = Resources.getResource("t_e802842950_e103778092.tst");
+
+        options.put(TestcaseProcessor.FILE_PATH_OPTION, tcFile.getPath());
 
         TestCase tc = proc.objectFromOptions(options);
         String json = proc.jsonFromObject(tc);
