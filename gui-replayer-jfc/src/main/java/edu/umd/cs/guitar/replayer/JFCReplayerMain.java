62c99690ef76c060dd0b7cdb733ecc4e0b79e991
Bryan Robbins <bryantrobbins@gmail.com>
2014-11-01 15:42:09 -0400

    Another argument compile error

--- gui-replayer-jfc/src/main/java/edu/umd/cs/guitar/replayer/JFCReplayerMain.java
+++ gui-replayer-jfc/src/main/java/edu/umd/cs/guitar/replayer/JFCReplayerMain.java
@@ -24,129 +24,131 @@
  */
 package edu.umd.cs.guitar.replayer;
 
-import java.io.FileNotFoundException;
+import edu.umd.cs.guitar.exception.GException;
+import edu.umd.cs.guitar.util.GUITARLog;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 
+import java.io.FileNotFoundException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-
-import edu.umd.cs.guitar.exception.GException;
-import edu.umd.cs.guitar.util.GUITARLog;
-
 /**
  * Entry class for JFCReplayer
- *
+ * <p/>
  * Note on exception handling:
- *
+ * <p/>
  * GUITAR related exceptions MUST be derived from GExceptions.
- *  All non GException exceptions are to be considered as AUT
- *  exceptions (unless explicitly stated and handled, in an
- *  itemised manner).
+ * All non GException exceptions are to be considered as AUT
+ * exceptions (unless explicitly stated and handled, in an
+ * itemised manner).
  *
- * 
  * @author <a href="mailto:baonn@cs.umd.edu"> Bao Nguyen </a>
  */
 public class JFCReplayerMain {
-   public static final int SUCCESS_EXIT = 0;
+    public static final int SUCCESS_EXIT = 0;
 
-   public static final int FILE_NOT_FOUND_EXCEPTION_EXIT = -3;
-   public static final int GUITAR_EXCEPTION_EXIT         = -2;
-   public static final int UNCAUCHT_EXCEPTION_EXIT       = -1;
+    public static final int FILE_NOT_FOUND_EXCEPTION_EXIT = -3;
+    public static final int GUITAR_EXCEPTION_EXIT = -2;
+    public static final int UNCAUCHT_EXCEPTION_EXIT = -1;
 
-   static long nStartTime;
-   static long nEndTime;
+    static long nStartTime;
+    static long nEndTime;
 
-   /**
-    * @param args
-    */
-   public static void
-   main(String[] args)
-   {
-      GUITARLog.log.info("------- BEGIN REPLAY -------");
-
-      int retVal = SUCCESS_EXIT;
-
-      nStartTime = System.currentTimeMillis();
-
-      /**
-       * Note: We don't actually use this configuration object. We only use
-       * this object to initialize the static fields of
-       * JFCReplayerConfiguration
-       */
-      JFCReplayerConfiguration configuration = new JFCReplayerConfiguration();
-      CmdLineParser parser = new CmdLineParser(configuration);
-      JFCReplayer jfcReplayer = new JFCReplayer(configuration);
-
-      try {
-         parser.parseArgument(args);
-         jfcReplayer.execute();
-
-         retVal = SUCCESS_EXIT;
-      } catch (CmdLineException e) {
-         System.err.println(e.getMessage());
-         System.err.println();
-         System.err
-               .println("Usage: java [JVM options] "
-                     + JFCReplayerMain.class.getName()
-                     + " [Relayer options] \n");
-
-         System.err.println("where [Replayer options] include:");
-         System.err.println();
-
-         parser.printUsage(System.err);
-         retVal = GUITAR_EXCEPTION_EXIT;
-
-      } catch (FileNotFoundException e) {
-
-         GUITARLog.log.error("File not found exception");
-         retVal = FILE_NOT_FOUND_EXCEPTION_EXIT;
-
-      } catch (GException e) {
-
-         GUITARLog.log.error("GUITAR exception");
-         retVal = GUITAR_EXCEPTION_EXIT;
-
-      } catch (Exception e) {
-
-         GUITARLog.log.error("General exception. " +
-                             "Likely AUT exception. " +
-                             "If not file GUITAR bug.");
-         retVal = UNCAUCHT_EXCEPTION_EXIT;
-
-         e.printStackTrace();
-      }
-
-      printInfo(retVal);
-
-      GUITARLog.log.info("-------- END REPLAY --------");
-
-      // Exit with error status
-      System.exit(retVal);
-   }
-
-   /**
-     * 
+    /**
+     * @param args
      */
-   static private void
-   printInfo(int retVal)
-   {
-      // ------------------
-      // Elapsed time:
-      long nEndTime = System.currentTimeMillis();
-      long nDuration = nEndTime - nStartTime;
-      DateFormat df = new SimpleDateFormat("HH : mm : ss: SS");
+    public static void
+    main(String[] args) {
+        GUITARLog.log.info("------- BEGIN REPLAY -------");
 
-      df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        int retVal = SUCCESS_EXIT;
 
-      GUITARLog.log.info("Pass status    : " + retVal);
-      GUITARLog.log.info("Time Elapsed   : " + df.format(nDuration));
-      GUITARLog.log.info("Testcase       : " + JFCReplayerConfiguration.TESTCASE);
-      GUITARLog.log.info("Log file       : " + JFCReplayerConfiguration.LOG_FILE);
-      GUITARLog.log.info("GUI state file : "
-            + JFCReplayerConfiguration.GUI_STATE_FILE);
-   }
+        nStartTime = System.currentTimeMillis();
+
+        /**
+         * Note: We don't actually use this configuration object. We only use
+         * this object to initialize the static fields of
+         * JFCReplayerConfiguration
+         */
+        JFCReplayerConfiguration configuration = new JFCReplayerConfiguration();
+        CmdLineParser parser = new CmdLineParser(configuration);
+        JFCReplayer jfcReplayer = new JFCReplayer(configuration);
+
+        try {
+            parser.parseArgument(args);
+            jfcReplayer.execute();
+
+            retVal = SUCCESS_EXIT;
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println();
+            System.err
+                    .println("Usage: java [JVM options] "
+                            + JFCReplayerMain.class.getName()
+                            + " [Relayer options] \n");
+
+            System.err.println("where [Replayer options] include:");
+            System.err.println();
+
+            parser.printUsage(System.err);
+            retVal = GUITAR_EXCEPTION_EXIT;
+
+        } catch (FileNotFoundException e) {
+
+            GUITARLog.log.error("File not found exception");
+            retVal = FILE_NOT_FOUND_EXCEPTION_EXIT;
+
+        } catch (GException e) {
+
+            GUITARLog.log.error("GUITAR exception");
+            retVal = GUITAR_EXCEPTION_EXIT;
+
+        } catch (Exception e) {
+
+            GUITARLog.log.error("General exception. " +
+                    "Likely AUT exception. " +
+                    "If not file GUITAR bug.");
+            retVal = UNCAUCHT_EXCEPTION_EXIT;
+
+            e.printStackTrace();
+        }
+
+        printInfo(retVal);
+
+        GUITARLog.log.info("-------- END REPLAY --------");
+
+        // Exit with error status
+        System.exit(retVal);
+    }
+
+    /**
+     *
+     */
+    static private void
+    printInfo(int retVal) {
+        // ------------------
+        // Elapsed time:
+        long nEndTime = System.currentTimeMillis();
+        long nDuration = nEndTime - nStartTime;
+        DateFormat df = new SimpleDateFormat("HH : mm : ss: SS");
+
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        GUITARLog.log.info("Pass status    : " + retVal);
+        GUITARLog.log.info("Time Elapsed   : " + df.format(nDuration));
+        GUITARLog.log.info("DB ID       : " +
+                JFCReplayerConfiguration.TESTDATA_DB_ID);
+        GUITARLog.log.info("Suite ID       : " +
+                JFCReplayerConfiguration.TESTDATA_SUITE_ID);
+        GUITARLog.log.info("Test ID       : " +
+                JFCReplayerConfiguration.TESTDATA_TEST_ID);
+
+        GUITARLog.log.info("Log file       : " + JFCReplayerConfiguration
+                .LOG_FILE);
+        GUITARLog.log.info("GUI state file : "
+                + JFCReplayerConfiguration.GUI_STATE_FILE);
+    }
 
 } // End of class
