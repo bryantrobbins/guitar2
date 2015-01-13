9652a7aefd6cd450a06d70ebc0dcc81e2dbf92c2
Bryan Robbins <bryantrobbins@gmail.com>
2015-01-12 22:56:29 -0500

    another syntax issue

--- dozen/tools/guitar/src/main/groovy/saveTCOutputs.groovy
+++ dozen/tools/guitar/src/main/groovy/saveTCOutputs.groovy
@@ -38,7 +38,7 @@ println fileList
 
 // Save each artifact
 fileList.eachWithIndex(){ path, i ->
-	println GOTHERE
+	println 'GOTHERE'
 	println path, i
 	opts = [(CoverageProcessor.FILE_PATH_OPTION): path, (CoverageProcessor.INDEX_OPTION): indexList[i] ]
 	manager.saveArtifact(cat, cp, opts, execId)
