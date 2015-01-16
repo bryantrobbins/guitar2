7f60829958d45bbfce977380597b5adfd44e59f9
guitar <guitar@guitar03.cs.umd.edu>
2015-01-15 23:37:59 -0500

    Enable image cleanup steps

--- config/image.sh
+++ config/image.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
-#docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave" | awk '{print $1}' | xargs docker stop
-#docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave" | awk '{print $1}' | xargs docker rm
-#docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave-linked" | awk '{print $1}' | xargs docker stop
-#docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave-linked" | awk '{print $1}' | xargs docker rm
+docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave" | awk '{print $1}' | xargs docker stop
+docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave" | awk '{print $1}' | xargs docker rm
+docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave-linked" | awk '{print $1}' | xargs docker stop
+docker ps -a | awk '{print $1,$2}' | grep "bryantrobbins/jslave-linked" | awk '{print $1}' | xargs docker rm
 
 # Build images for jenkins slaves
-#docker rmi bryantrobbins/jslave
+docker rmi bryantrobbins/jslave
 docker build -t="bryantrobbins/jslave" ./slave
 
-#docker rmi bryantrobbins/jslave-linked
+docker rmi bryantrobbins/jslave-linked
 docker build -t="bryantrobbins/jslave-linked" ./slave-linked
