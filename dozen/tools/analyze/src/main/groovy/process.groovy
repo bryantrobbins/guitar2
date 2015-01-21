ad731a3ad70220377bb380db6a07dcf349efb6f2
Bryan Robbins <bryantrobbins@gmail.com>
2015-01-21 07:53:25 -0500

    Move log file processing to external script

--- dozen/tools/analyze/src/main/groovy/process.groovy
+++ dozen/tools/analyze/src/main/groovy/process.groovy
@@ -1,6 +1,7 @@
-// HOST, PORT, DB_ID, BUNDLE_ID
+// HOST, PORT, DB_ID, BUNDLE_ID, SUITE_ID
 
 import edu.umd.cs.guitar.main.TestDataManager
+import edu.umd.cs.guitar.main.TestDataManagerCollections
 import edu.umd.cs.guitar.processors.guitar.LogProcessor
 import edu.umd.cs.guitar.artifacts.ArtifactCategory
 import edu.umd.cs.guitar.processors.applog.TextObject
@@ -9,30 +10,49 @@ String host = args[0]
 String port = args[1]
 String dbId = args[2]
 String bundleId = args[3]
+String suiteId = args[4]
 
 // TestDataManager
 def manager = new TestDataManager(host, port, dbId)
 def logProc = new LogProcessor()
 
 println "Analyzing bundle " + bundleId
+println "The collection is " + TestDataManagerCollections.idsInBundle(bundleId)
 
 List<String> failing = []
 List<String> passing = []
-for(String execId : manager.getExecutionIdsInBundle(bundleId)){
-	// Get log object
+List<String> missingLog = []
+List<String> missingExec = []
+
+def testIds = manager.getTestIdsInSuite(suiteId)
+println "There are " + testIds.size() + " tests in suite " + suiteId
+
+for(String testId : manager.getTestIdsInSuite(suiteId)){
+	String execId = manager.getExecutionIdForTestIdInBundle(bundleId, testId)
+	
+	if(execId == null){
+		missingExec.add(testId)
+		continue
+	}
+
 	TextObject logObject = manager.getArtifactByCategoryAndOwnerId(ArtifactCategory.TEST_OUTPUT,
-                                                  execId, logProc)
-	if(!pass(logObject)){
-		failing.add(execId)
-	} else{
-		passing.add(execId)
+																																 execId, logProc)
+	if(logObject == null){
+		missingLog.add(testId)
+	} else {
+		boolean didPass = pass(logObject)
+		if(didPass){
+			passing.add(testId)
+		} else{
+			failing.add(testId)
+		}
 	}
 }
 
 def pass(obj){
 	for(int i=0; i<obj.size(); i++){
 		String line = obj.getLine(i)
-		if (line.toUpper().contains("ERROR")){
+		if (line.toUpperCase().contains("ERROR")){
 			return false
 		}
 	}
@@ -42,6 +62,12 @@ def pass(obj){
 println "========FAILING========"
 println "Failing test count=" + failing.size()
 println failing
+println "========ERROR-E========"
+println "Error execution missing count=" + missingExec.size()
+println missingExec
+println "========ERROR-L========"
+println "Error log missing count=" + missingLog.size()
+println missingLog
 println "========PASSING========"
 println "Passing test count=" + passing.size()
 println passing
--- dozen/tools/guitar/src/main/groovy/saveTCOutputs.groovy
+++ dozen/tools/guitar/src/main/groovy/saveTCOutputs.groovy
@@ -2,6 +2,7 @@
 
 import edu.umd.cs.guitar.main.TestDataManager
 import edu.umd.cs.guitar.processors.guitar.CoverageProcessor
+import edu.umd.cs.guitar.processors.guitar.LogProcessor
 import net.sourceforge.cobertura.coveragedata.ProjectData
 import edu.umd.cs.guitar.util.CoberturaUtils
 import edu.umd.cs.guitar.artifacts.ArtifactCategory
@@ -21,10 +22,13 @@ def manager = new TestDataManager(host, port, dbId)
 // Prepare args
 ArtifactCategory cat = ArtifactCategory.TEST_OUTPUT
 CoverageProcessor cp = new CoverageProcessor(manager.getDb())
+LogProcessor lp = new LogProcessor()
 
 // Get list of coverage files from coverageDir
 fileList = []
 indexList = []
+def logFile = null
+
 def dir = new File(coverageDir)
 dir.eachFileRecurse (FileType.FILES) { file ->
   def splits = file.getName().toString().split("\\.")
@@ -32,6 +36,9 @@ dir.eachFileRecurse (FileType.FILES) { file ->
     indexList << file.getName().toString().split("\\.")[0]
     fileList << file.getAbsolutePath()
   }
+	if(splits[-1].equals("log")){
+		logFilePath = file.getAbsolutePath()
+	}
 }
 
 println fileList
@@ -41,3 +48,7 @@ fileList.eachWithIndex(){ path, i ->
 	def opts = [(CoverageProcessor.FILE_PATH_OPTION): path, (CoverageProcessor.INDEX_OPTION): indexList[i] ]
 	manager.saveArtifact(cat, cp, opts, execId)
 }
+
+// Save log artifact
+def opts = [(LogProcessor.FILE_PATH_OPTION): logFilePath]
+manager.saveArtifact(cat, lp, opts, execId)
