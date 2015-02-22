30794fcc01b883d4c9653fe0e0bc6d2a595a2205
Bryan Robbins <bryantrobbins@gmail.com>
2015-02-21 23:56:39 -0500

    Rolling back to guitar03

--- dozen/tools/guitar/src/main/groovy/suiteReplay.groovy
+++ dozen/tools/guitar/src/main/groovy/suiteReplay.groovy
@@ -3,7 +3,7 @@
 import edu.umd.cs.guitar.util.JenkinsClient
 import edu.umd.cs.guitar.main.TestDataManager
 
-def master = "172.16.5.255"
+def master = "guitar03.cs.umd.edu"
 
 // Jenkins client
 def jenkinsClient = new JenkinsClient(master, "8080", "", "admin", "amalga84go")
