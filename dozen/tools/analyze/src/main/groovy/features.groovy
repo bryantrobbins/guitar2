e902fd46e12b4773b2afafd28246127db0a7eaa8
Bryan Robbins <bryantrobbins@gmail.com>
2015-07-12 08:13:52 -0400

    Adding properties file to features.groovy

--- dozen/tools/analyze/src/main/groovy/features.groovy
+++ dozen/tools/analyze/src/main/groovy/features.groovy
@@ -9,9 +9,18 @@ def suites = Arrays.asList(args[3].split(","))
 int maxN = Integer.parseInt(args[4])
 println args
 
+// Save some properties of this execution
+Properties groupIds = new Properties();
+
 for(n in 0..maxN+1) {
 	println "N=${n}"
 
 	// Extract features and post to DB
-	ExperimentManager.addGlobalFeaturesForSuites(host, port, dbId, suites, n)
+	def id = ExperimentManager.addGlobalFeaturesForSuites(host, port, dbId, suites, n)
+
+	// Save the group ID for future reference
+	groupIds.setProperty("features_n_${n}", id)	
 }
+
+// Dump properties
+groupIds.store(new FileOutputStream("groups.properties", null);
