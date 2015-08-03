cf3da9a3289135a329ecee90e44661f95aeba2b8
Bryan Robbins <bryantrobbins@gmail.com>
2015-08-02 20:09:09 -0400

    First shot at tuning grid script

---
+++ paper/src/main/groovy/tuningGrid.groovy
@@ -0,0 +1,31 @@
+// ARGS: DATASET, MIN_GAMMA, MAX_GAMMA, MIN_COST, MAX_COST
+
+import edu.umd.cs.guitar.util.JenkinsClient
+import edu.umd.cs.guitar.main.TestDataManager
+
+def master = "guitar05.cs.umd.edu"
+def dataset = args[0]
+def minGammaExp = args[1]
+def maxGammaExp = args[2]
+def minCostExp = args[3]
+def maxCostExp = args[4]
+
+// Jenkins client
+def jenkinsClient = new JenkinsClient(master, "8888", "", "admin", "amalga84go")
+
+for (int gamma=minGammaExp; gamma<=maxGammaExp; i++) {
+	for (int cost=minCostExp; cost<=maxCostExp; i++) {
+   	// build Map of params
+   	// I have only used text params, but perhaps others supported via Jenkins Remote API
+   	def jobParams = new HashMap<String, String>();
+   	jobParams.put("DATASET", dataset)
+   	jobParams.put("GAMMA_EXPONENT", gamma)
+   	jobParams.put("COST_EXPONENT", cost)
+
+  	// Use Jenkins client to launch job
+   	jenkinsClient.submitJob("train-model", jobParams)
+
+		// ZZZ to let the master recover
+		sleep(3000)
+	}
+}
