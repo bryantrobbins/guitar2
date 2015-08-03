d862fb6426fd3994a996e92574faee3722e017b4
Bryan Robbins <bryantrobbins@gmail.com>
2015-08-02 20:30:39 -0400

    Integer parsing

--- paper/src/main/groovy/tuningGrid.groovy
+++ paper/src/main/groovy/tuningGrid.groovy
@@ -5,10 +5,10 @@ import edu.umd.cs.guitar.main.TestDataManager
 
 def master = "guitar05.cs.umd.edu"
 def dataset = args[0]
-def minGammaExp = args[1]
-def maxGammaExp = args[2]
-def minCostExp = args[3]
-def maxCostExp = args[4]
+def minGammaExp = Integer.parseInt(args[1])
+def maxGammaExp = Integer.parseInt(args[2])
+def minCostExp = Integer.parseInt(args[3])
+def maxCostExp = Integer.parseInt(args[4])
 
 // Jenkins client
 def jenkinsClient = new JenkinsClient(master, "8888", "", "admin", "amalga84go")
@@ -18,9 +18,9 @@ for (int gamma=minGammaExp; gamma<=maxGammaExp; i++) {
    	// build Map of params
    	// I have only used text params, but perhaps others supported via Jenkins Remote API
    	def jobParams = new HashMap<String, String>();
-   	jobParams.put("DATASET", dataset)
-   	jobParams.put("GAMMA_EXPONENT", gamma)
-   	jobParams.put("COST_EXPONENT", cost)
+   	jobParams.put("DATASET", dataset.toString())
+   	jobParams.put("GAMMA_EXPONENT", gamma.toString())
+   	jobParams.put("COST_EXPONENT", cost.toString())
 
   	// Use Jenkins client to launch job
    	jenkinsClient.submitJob("train-model", jobParams)
