49011e646e4471ee07b06919cc2cab683ff53c2d
Bryan Robbins <bryantrobbins@gmail.com>
2015-11-21 12:43:10 -0500

    Removing unused R scripts

--- paper/rscripts/csv.r
+++
@@ -1,9 +0,0 @@
-library("e1071", lib.loc="/opt/Rpackages/")
-input <- data.frame(read.csv("data/xor.csv"))
-y <- input[,2]
-x <- input
-x$id <- NULL
-x$y <- NULL
-model <- svm(x,y,type="C-classification",kernel="radial")
-print(model)
-predict(model,x)
